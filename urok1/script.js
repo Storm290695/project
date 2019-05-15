a68fe2ab88db142ebb2ee60ac96d2b45121fdfb7
Storm <psiholog290695@gmail.com>
2019-05-16 01:28:42 +0300

    Это наш первый комит

---
+++ urok1/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+
+
+
+console.log(4/0);
+console.log('string'*9);
+let something;
+console. log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['we.png','orange.png','aple.jpg'];
+
+console.log(arr[0]);
+
+let answer = prompt("Сколько вам лет?", "");
+ console.log(typeof(answer)); 
